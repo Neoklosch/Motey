b2b7c6ec33e67e7a6678c7b0db9df8f427675887
neoklosch <neoklosch@neoklosch-ThinkPad-X1-Carbon-4th.fritz.box>
2017-05-04 16:36:51 +0200

    Added docker container scripts.

---
+++ fog-node-container/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+apt-get update && apt-get -y upgrade && apt-get install -y curl nano locate man python3 python3-pip build-essential libssl-dev libffi-dev python-dev aufs-tools cgroupfs-mount cgroup-lite git apparmor
+apt-get install -y --force-yes apt-transport-https
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+echo "deb https://download.docker.com/linux/ubuntu xenial stable" >> /etc/apt/sources.list
+apt-get update
+apt-get install -y docker-ce
