f4e2c9fc565442069638659d7b5b74c00d3a10e1
neoklosch <neoklosch@neoklosch-ThinkPad-X1-Carbon-4th.fritz.box>
2017-05-04 19:07:30 +0200

    Modified docker script.

--- scripts/docker_stuff.sh
+++ scripts/docker_stuff.sh
@@ -4,10 +4,10 @@
 docker build -t neoklosch/fog_node_prototype .
 
 # commit image
-docker commit bbd42c2ce2a4 neoklosch/fog_node_prototype
+docker commit 788a449e4063 neoklosch/fog_node_prototype
 
 # upload image
 docker push neoklosch/fog_node_prototype
 
 # start the container with shared folder
-docker run -ti -v /home/neoklosch/latex-projects/fog_node_prototype:/opt/docker-stuff
+docker run -ti -v /var/run/docker.sock:/var/run/docker.sock -v /home/neoklosch/latex-projects/fog_node_prototype:/opt/docker-stuff neoklosch/fog_node_prototype
