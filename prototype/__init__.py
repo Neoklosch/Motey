04b5829a5555b988b76da6b01d6bcdf19ccb963f
neoklosch <neoklosch@neoklosch-ThinkPad-X1-Carbon-4th.fritz.box>
2017-05-06 20:53:46 +0200

    Removed useless ExitStatus class.

--- prototype/__init__.py
+++ prototype/__init__.py
@@ -3,13 +3,3 @@ fog node prototype
 """
 __version__ = '0.0.1'
 __author__ = 'Markus Paeschke'
-
-
-class ExitStatus:
-    """Exit status code constants."""
-    OK = 0
-    ERROR = 1
-    PLUGIN_ERROR = 7
-
-    # 128+2 SIGINT <http://www.tldp.org/LDP/abs/html/exitcodes.html>
-    ERROR_CTRL_C = 130
