eea66eef69507b9d727def9ee241287eadbde0d0
Neoklosch <markus.paeschke@gmail.com>
2017-05-10 23:58:41 +0200

    Testing stuff

---
+++ tests/test_localorchestrator.py
@@ -0,0 +1,15 @@
+import unittest
+
+
+class TestLocalOrchestrator(unittest.TestCase):
+
+    @classmethod
+    def setUp(self):
+        pass
+
+    def test_handle_blueprint(self):
+        self.assertEqual(1, 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
