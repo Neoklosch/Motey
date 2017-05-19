4be0ca462baea3bf83d30f897271d2572261b0ed
Neoklosch <markus.paeschke@gmail.com>
2017-05-19 17:29:27 +0200

    Improved cli tool.

--- fog_node_engine/cli/main.py
+++ fog_node_engine/cli/main.py
@@ -4,18 +4,20 @@
 Fog Node Prototype command line tool.
 
 Usage:
-  fognode start
+  fognode start [-d | --daemon]
   fognode stop
   fognode -h | --help
   fognode --version
 
  Options:
+   -d, --daemon     Start as services as daemon.
    -h, --help       Show this message.
    --version        Print the version.
 """
 
 from docopt import docopt
-from inspect import getmembers, isclass
+import fog_node_engine
+from fog_node_engine.core import Core
 # from fog_node_engine import __version__ as VERSION
 
 def main():
@@ -23,6 +25,10 @@ def main():
     print(options)
 
     if options['start']:
+        if options['--daemon']:
+            print("start as daemon")
+        else:
+            print("start normal")
         print("starting my shit")
     elif options['stop']:
         print('stopping my shit')
