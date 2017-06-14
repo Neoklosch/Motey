25ade7600c782633fa28b786236162f0c4bd2833
Neoklosch <markus.paeschke@gmail.com>
2017-06-14 22:20:13 +0200

    Test path thingy.

--- motey/configuration/configreader.py
+++ motey/configuration/configreader.py
@@ -1,6 +1,4 @@
 import configparser
 
-from motey.utils.path_helper import absolute_file_path
-
 config = configparser.ConfigParser()
-config.read(absolute_file_path('motey/configuration/config.ini'))
+config.read('motey/configuration/config.ini')
