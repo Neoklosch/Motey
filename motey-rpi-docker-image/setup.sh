735ef4f694f9c669a91ae46f2739b57364732d5c
Neoklosch <markus.paeschke@gmail.com>
2017-07-06 21:33:20 +0200

    Modified the rpi setup script.

--- motey-rpi-docker-image/setup.sh
+++ motey-rpi-docker-image/setup.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
 
 apt-get update
-apt-get -y upgrade && apt-get install -y curl nano locate man python3 python3-pip build-essential libssl-dev libffi-dev python-dev aufs-tools cgroupfs-mount cgroup-lite git apparmor
+apt-get -y upgrade && apt-get install -y curl nano locate man build-essential libssl-dev libffi-dev python-dev aufs-tools cgroupfs-mount git apparmor wget
 apt-get install -y --force-yes apt-transport-https
+apt-get prune -y python3 python3-pip
 curl -sSL get.docker.com | sh
+
+cd /tmp
+wget https://www.python.org/ftp/python/3.5.2/Python-3.5.2.tgz
+tar -zxvf Python-3.5.2.tgz
+cd /tmp/Python-3.5.2
+./configure
+make
+make install
+python3 --version
 
 pip3 install --upgrade pip
 pip3 install -r /tmp/requirements.txt
